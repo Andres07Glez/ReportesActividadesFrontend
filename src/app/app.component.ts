2c6e1f82b382d86fe8efa36946715e7c05e80dce
Andres07Glez <manuel07andres@gmail.com>
2025-06-16 10:52:33 -0600

    add plantilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThesisDirComponent } from "./components/ThesisDir/thesis-dir/thesis-dir.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ThesisDirComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
